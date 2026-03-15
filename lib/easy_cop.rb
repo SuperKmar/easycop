a50e4bd688e31eab2b650f92adaf55ee86e452d1
Alex Acht <alexakhtyrskii@gmail.com>
2026-03-15 12:31:26 +0300

    Fix TODO: result is true | false with lazy evaluation, we need an arrey of offenses

--- lib/easy_cop.rb
+++ lib/easy_cop.rb
@@ -1,24 +1,45 @@
+# frozen_string_literal: true
+
 require 'rubocop'
 
+class CollectingRunner < RuboCop::Runner
+  attr_reader :offenses_by_file
+
+  def initialize(options = {}, config_store = RuboCop::ConfigStore.new)
+    super(options, config_store)
+    @offenses_by_file = Hash.new { |h, k| h[k] = [] }
+  end
+
+  private
+
+  def file_finished(file, offenses)
+    @offenses_by_file[file].concat(offenses)
+    super
+  end
+end
+
 def find_least_offended_file
-
   config_store = RuboCop::ConfigStore.new
-  result = RuboCop::Runner.new({}, config_store).run([])
-  # TODO: result is true | false with lazy evaluation, we need an arrey of offenses
-  offenses_by_file = Hash.new(0)
+  runner       = CollectingRunner.new({}, config_store)
+  runner.run([])
 
-  result.offenses.each do |offense|
-    file = offense.location.file
-    offenses_by_file[file] += 1
-  end
+  offenses_count_by_file = runner.offenses_by_file
+                                 .transform_values(&:size)
+                                 .reject { |_file, count| count.zero? }
 
-  if offenses_by_file.empty?
-    puts 'No offenses found. All files are clean!'
-  else
-    min_file = offenses_by_file.min_by { |_file, count| count }
-    file_path = File.relative_path(min_file[0], Dir.pwd)
-    puts "The file with the least infractions is '#{file_path}' with #{min_file[1]} offense(s)."
-  end
+  return puts 'No offenses found. All files are clean!' if offenses_count_by_file.empty?
+
+  min_file, count = offenses_count_by_file.min_by { |_file, c| c }
+  file_path = Pathname.new(min_file).relative_path_from(Pathname.pwd).to_s
+
+  puts <<-TEXT
+
+        |                           |                             |
+        |                           |                             |
+       \\ /                         \\ /                           \\ /
+        v                           v                             v
+  TEXT
+  puts "\nThe file with the least infractions is '#{file_path}' with #{count} offense(s)."
 end
 
 find_least_offended_file
